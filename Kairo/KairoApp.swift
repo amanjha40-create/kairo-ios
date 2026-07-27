b14f6fe67d98afc02901f49a1ea862bc2f33b8b3
Aman Jha <amanjha40@gmail.com>
2026-07-27 19:08:11 +0530

    Initial Commit

---
+++ Kairo/KairoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KairoApp.swift
+//  Kairo
+//
+//  Created by Aman on 27/07/26.
+//
+
+import SwiftUI
+
+@main
+struct KairoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
